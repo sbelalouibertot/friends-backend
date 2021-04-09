ab9bf4afd0272452b327dac200c2c96ca76795f3
Samy Belaloui-Bertot <samy@MacBook-Air-de-user.local>
2021-04-09 18:18:51 +0000

    add-activity endpoints + sorting

--- api/routes/feed.js
+++ api/routes/feed.js
@@ -16,7 +16,7 @@ router.get("/all", (req, res, next) => {
       avatar: users.find((user) => user.id === post.userId)?.profileImg,
     },
     ...post,
-  }));
+  })).sort((a, b) => b.id - a.id);
   res.status(200).send(feed);
 });
 
@@ -37,7 +37,7 @@ router.post("/like", (req, res, next) => {
   if (index === -1) ids.push(sourceUserId);
   else ids.splice(index, 1);
   posts[postId].reactions.likes.userIds = ids;
-  
+
   writeData("posts", posts);
 
   const users = loadData("users");
@@ -47,7 +47,45 @@ router.post("/like", (req, res, next) => {
       avatar: users.find((user) => user.id === post.userId)?.profileImg,
     },
     ...post,
-  }));
+  })).sort((a, b) => b.id - a.id);
+
+  res.status(200).send(feed);
+});
+
+/**
+ * POST http://localhost:3001/feed/add
+ * Request body :
+ * activity -> key of activity. e.g: RESTAURANT
+ * sourceUserId -> id of the user that is adding a new post
+ * returns the updated feed
+ */
+router.post("/add", (req, res, next) => {
+  const posts = loadData("posts");
+  const { activity, sourceUserId } = req.body;
+
+  const newPost = {
+    id: posts.length,
+    userId: sourceUserId,
+    date: new Date(),
+    activity,
+    reactions: {
+      likes: {
+        userIds: [],
+      },
+    },
+  };
+
+  posts.push(newPost);
+  writeData("posts", posts);
+
+  const users = loadData("users");
+  const feed = posts.map((post) => ({
+    ...{
+      userName: users.find((user) => user.id === post.userId)?.firstName,
+      avatar: users.find((user) => user.id === post.userId)?.profileImg,
+    },
+    ...post,
+  })).sort((a, b) => b.id - a.id);
 
   res.status(200).send(feed);
 });
