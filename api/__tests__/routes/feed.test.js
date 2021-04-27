dccc20f2f99774692e1c7ed0ff9d7e0d48354c1c
Samy Belaloui-Bertot <samy.belalouibertot@gmail.com>
2021-04-27 07:54:09 +0200

    unit tests

--- api/__tests__/routes/feed.test.js
+++ api/__tests__/routes/feed.test.js
@@ -1,7 +1,21 @@
 const supertest = require("supertest");
 const { app } = require("../../../app");
 
-test("GET /", async () => {
+test("GET /feed/all", async () => {
   const feed = await supertest(app).get("/feed/all");
-  console.log("feed.body = ", feed.body);
+  const feedItem = feed.body[0];
+
+  expect(feed.status).toBe(200);
+  expect(feedItem).toHaveProperty("id");
+  expect(feedItem).toHaveProperty("userName");
+  expect(feedItem).toHaveProperty("userId");
+  expect(feedItem).toHaveProperty("avatar");
+  expect(feedItem).toHaveProperty("date");
+  expect(feedItem).toHaveProperty("activity");
+  expect(feedItem).toHaveProperty("reactions");
+
+  expect(feedItem.reactions).toHaveProperty("likes");
+  expect(feedItem.reactions.likes).toHaveProperty("userIds");
+
+  expect(Array.isArray(feedItem.reactions.likes.userIds)).toBeTruthy()
 });
--- api/__tests__/routes/users.test.js
+++ api/__tests__/routes/users.test.js
@@ -1,7 +1,83 @@
 const supertest = require("supertest");
 const { app } = require("../../../app");
 
-test("GET /", async () => {
-  const users = await supertest(app).get("/users/all");
-  console.log("users.body = ", users.body);
+describe("Users", () => {
+  test("GET /users/all", async () => {
+    const users = await supertest(app).get("/users/all");
+    const user = users.body[0];
+
+    expect(users.status).toBe(200);
+    expect(user).toHaveProperty("id");
+    expect(user).toHaveProperty("firstName");
+    expect(user).toHaveProperty("lastName");
+    expect(user).toHaveProperty("gender");
+    expect(user).toHaveProperty("birthDate");
+    expect(user).toHaveProperty("city");
+    expect(user).toHaveProperty("profileImg");
+    expect(user).toHaveProperty("subscriptionDate");
+    expect(user).toHaveProperty("friendsIds");
+    expect(user).toHaveProperty("photos");
+    expect(user).toHaveProperty("notifications");
+
+    expect(Array.isArray(user.friendsIds)).toBeTruthy();
+    expect(Array.isArray(user.photos)).toBeTruthy();
+    expect(Array.isArray(user.notifications)).toBeTruthy();
+  });
+
+  test("GET /users/0", async () => {
+    const userId = 0
+    const userResponse = await supertest(app).get(`/users/${userId}`);
+    expect(userResponse.status).toBe(200);
+
+    const user = userResponse.body;
+    expect(user).toHaveProperty("id");
+    expect(user.id).toBe(0)
+    expect(user).toHaveProperty("firstName");
+    expect(user).toHaveProperty("lastName");
+    expect(user).toHaveProperty("gender");
+    expect(user).toHaveProperty("birthDate");
+    expect(user).toHaveProperty("city");
+    expect(user).toHaveProperty("profileImg");
+    expect(user).toHaveProperty("subscriptionDate");
+    expect(user).toHaveProperty("friendsIds");
+    expect(user).toHaveProperty("photos");
+    expect(user).toHaveProperty("notifications");
+
+    expect(Array.isArray(user.friendsIds)).toBeTruthy();
+    expect(Array.isArray(user.photos)).toBeTruthy();
+    expect(Array.isArray(user.notifications)).toBeTruthy();
+
+  });
+
+  test("GET /users/0/photos", async () => {
+    const userId = 0
+    const response = await supertest(app).get(`/users/${userId}/photos`);
+    expect(response.status).toBe(200);
+
+    const photos = response.body;
+    expect(Array.isArray(photos)).toBeTruthy()
+
+    const photo = photos[0]
+    expect(photo).toHaveProperty("id");
+    expect(photo.id).toBe(0)
+    expect(photo).toHaveProperty("source");
+    expect(photo).toHaveProperty("uploadedDate");
+    expect(photo).toHaveProperty("ownerId");
+  });
+  
+  test("GET /users/0/photos", async () => {
+    const userId = 0
+    const response = await supertest(app).get(`/users/${userId}/photos`);
+    expect(response.status).toBe(200);
+
+    const photos = response.body;
+    expect(Array.isArray(photos)).toBeTruthy()
+
+    const photo = photos[0]
+    expect(photo).toHaveProperty("id");
+    expect(photo.id).toBe(0)
+    expect(photo).toHaveProperty("source");
+    expect(photo).toHaveProperty("uploadedDate");
+    expect(photo).toHaveProperty("ownerId");
+  });
 });
